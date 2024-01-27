e012b82a3306aeb507208147cf535d3ce00def1f
ademnsir <nsir.adem@esprit.tn>
2024-01-27 20:28:59 +0100

    update2

--- src/pages/QuickConsultationPage.jsx
+++ src/pages/QuickConsultationPage.jsx
@@ -6,6 +6,17 @@ function QuickConsultationPage() {
   return (
     <div>
       {/* Contenu de votre page QuickConsultation */}
+     
+      
+      {/* Ajoutez une image avec des dimensions personnalisées */}
+      <img
+        src="/img/back1.jpg" // Remplacez par le chemin de votre image
+        alt="Description de l'image"
+        style={{
+          width: "100%", // Largeur à 100% de la largeur parente
+          height: "400px", // Hauteur fixe à 200 pixels
+        }}
+      />
     </div>
   );
 }
