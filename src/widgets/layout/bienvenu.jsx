4faf614a7b8b83ceaaf792326573099f0f725276
mohamedaziztemimi <mohamedaziz.temimi@esprit.tn>
2024-01-29 19:34:56 +0100

    annimation

--- src/widgets/layout/bienvenu.jsx
+++ src/widgets/layout/bienvenu.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import Lottie from 'lottie-react'
+import AnimationData from "../assets/Animation.json";
 
 export  function Bienvenu() {
   return (
     <>
     <section>
      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-   
+     <Lottie className="mx-auto mt-1 max-w-md" style={ { height: 150, width: 150 }} animationData={AnimationData} /> 
            <header className="text-center">
            <p className="mx-auto mt-1 max-w-md text-gray-500">
                 28/01/2024
              </p>
-                <h3 className="text-xl font-bold text-gray-900 sm:text-3xl">Hello,Aziz</h3>    
+                <h3 className="text-xl font-bold text-gray-900 sm:text-3xl">Hello,Aziz   </h3>
            </header>
+           
+    
+        
            
      </div>
     </section>
