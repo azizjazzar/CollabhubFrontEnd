89383b4ad584a0d8dae0720a865fce4e187236c0
ademnsir <nsir.adem@esprit.tn>
2024-02-18 19:26:28 +0100

    gestionconsultation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,9 +15,7 @@ import { PageTitle, Footer } from "@/index";
 import { FeatureCard, TeamCard } from "@/widgets/cards";
 import { featuresData, teamData, contactData } from "@/data";
 import { ReactTyped } from "react-typed";
-
 import { useNavigate } from 'react-router-dom';
-
 
 export function Home() {
   const [backgroundImage, setBackgroundImage] = useState('/img/back1.jpg');
@@ -69,12 +67,9 @@ export function Home() {
               <Typography variant="lead" color="white" className="opacity-80 pt-20">
                 Forget the hurdles in collaboration with our experts on CollabHub, achieving excellence is simpler than ever.
               </Typography>
-            
-          
             </div>
           </div>
           <div className="my-24 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3" style={{ userSelect: 'none' }}>
-
             {/* Bouton 1 */}
             <div className="group border p-4 rounded-lg cursor-pointer -mb-6 hover:bg-orange-400 hover:text-white" onClick={() => handleButtonClick(1)}>
               <p className="text-white text-4xl font-bold mb-2">Post a job and hire a Pro</p>
@@ -91,10 +86,8 @@ export function Home() {
             <div className="group border p-4 rounded-lg cursor-pointer -mb-6 hover:bg-orange-400 hover:text-white" onClick={() => handleButtonClick(3)}>
               <p className="text-white text-4xl font-bold mb-2">Get advice from an industry expert</p>
               <p className="text-orange-400 text-xl group-hover:text-white pt-3">Consultations</p>
-
             </div>
-
-
+          </div>
         </div>
       </div>
 
@@ -152,7 +145,6 @@ export function Home() {
               </Card>
             ))}
           </div>
-
         </div>
       </section>
 
