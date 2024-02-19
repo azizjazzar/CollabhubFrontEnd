352a8d898685208187b74809c6628ea736de7ccf
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-19 02:22:57 +0100

    changeess

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+    alias: [{ find: "@", replacement: "/src" }],
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/widgets/assets/animation.scss";`,
-      },
-    },
+  scss: {
+    additionalData: `@import "./src/widgets/assets/animation.scss";`,
   },
 });
