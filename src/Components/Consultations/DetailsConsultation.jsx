412d4d5271542136d925858e983120bfda8033d0
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-19 02:06:36 +0100

    changeess

--- src/Components/Consultations/DetailsConsultation.jsx
+++ src/Components/Consultations/DetailsConsultation.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import CardsConsultations from '../../widgets/cards/cardsPayerConsultations.jsx';
-import InformationDetailsCons from '../../widgets/cards/InformationDetailsCons.jsx';
-import { Footer } from "../../widgets/layout/footer";
+import CardsConsultations from '@/widgets/cards/cardsPayerConsultations.jsx';
+import InformationDetailsCons from '@/widgets/cards/InformationDetailsCons.jsx';
+import { Footer } from "@/widgets/layout/footer";
 import { FaVideo } from 'react-icons/fa';
 import { loadStripe } from '@stripe/stripe-js';
 import { useAuth } from "@/pages/authContext";
