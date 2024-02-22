a939b49023301a9da10b2d65d72fdec610dc09cc
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-22 08:33:43 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export * from "@/sidebar";
+export * from "@/widgets/layout/sidebar";
 export * from "@/widgets/layout/footer";
 export * from "@/widgets/layout/simple-footer";
 export * from "@/widgets/layout/page-title";
@@ -6,7 +6,7 @@ export * from "@/widgets/layout/bienvenu";
 export * from "@/widgets/layout/information-section";
 export * from "@/widgets/layout/macarousel";
 export * from "@/widgets/layout/Modal";
-export * from "@/widgets/layout/serviceTitle";
+export * from "@/ServiceTitle";
 export * from "@/widgets/layout/TitleConsultations";
 export * from "@/widgets/layout/SideBarConsultations";
 export * from "@/widgets/layout/formulaireConsultation";
