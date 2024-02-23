32f3755ed7828abde102d81e2df9ed66b31d8ca8
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-23 03:38:44 +0100

    Migration

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export * from "@/pages/home";
-export * from "@/pages/profile";
-export * from "@/pages/sign-in";
-export * from "@/pages/sign-up";
-export * from "@/pages/collaboration";
-export * from "@/pages/freelancer-collab";
-export * from "@/Components/Consultations/QuickConsultationPage";
-export * from "@/pages/project-collab";
-
--- src/widgets/cards/index.js
+++
@@ -1,7 +0,0 @@
-export * from "@/widgets/cards/feature-card";
-export * from "@/widgets/cards/team-card";
-export * from "@/widgets/cards/cardsgrids";
-export * from "@/widgets/cards/info-card";
-export * from "@/widgets/cards/cardsConsultations";
-export * from "@/widgets/cards/InfoCard"; 
-
