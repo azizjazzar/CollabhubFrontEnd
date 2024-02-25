9877c7523c99939d231a61925e23da9ed3e26d85
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-25 13:46:44 +0100

    updates in voice

--- src/Components/Authentification/Alan.jsx
+++ src/Components/Authentification/Alan.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import TextToSpeech from "./text-to-speech/TextToSpeech";
 import SpeechToText from "./speech-to-text/SpeechToText ";
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../pages/authContext';
 
 export default function Alan() {
   const [transcript, setTranscript] = useState("");
@@ -12,12 +13,44 @@ export default function Alan() {
   const [scroll, setScroll] = useState(0);
   const [titleService, setTitles] = useState(false);
   const [newsPromptSpoken, setNewsPromptSpoken] = useState(false);
-
+  const [actor, setActor] = useState(2);
   const location = useLocation();
+  const { authData, setAuthUserData } = useAuth();
+  const [logoutSure, setLogoutSure ] = useState(false);
+
+  const logout = () => {
+    setAuthUserData({
+      user: null,
+      accessToken: null,
+      refreshToken: null,
+    });
+
+    localStorage.setItem('authData', JSON.stringify({
+      user: null,
+      accessToken: null,
+      refreshToken: null,
+    }));
+  };
 
   const handleTranscriptChange = (newTranscript) => {
     setTranscript(newTranscript.toUpperCase());
     setSpeech(null);
+  };
+
+  const handleLogout = () => {
+  
+    setAuthUserData({
+      user: null,
+      accessToken: null,
+      refreshToken: null,
+    });
+
+    localStorage.setItem('authData', JSON.stringify({
+      user: null,
+      accessToken: null,
+      refreshToken: null,
+    }));
+    navigate("/");
   };
 
   useEffect(() => {
@@ -25,9 +58,7 @@ export default function Alan() {
     if (storedConsultations) {
       setConsultations(JSON.parse(storedConsultations));
     }
-  }, [location.pathname]);
 
-  useEffect(() => {
     const storedServices = localStorage.getItem("services");
     if (storedServices) {
       setServices(JSON.parse(storedServices));
@@ -36,118 +67,160 @@ export default function Alan() {
 
   useEffect(() => {
     if (!speech) {
-      if (transcript === "GO TO SERVICE") {
-        setSpeech({
+      let action = null;
+
+      if (transcript.includes("GO TO SERVICE")) {
+        action = {
           text: "Yes, going right now.",
           language: "en-US",
-          voiceIndex: 5
-        });
+          voiceIndex: actor
+        };
+        const destination = transcript.includes("GO TO SERVICES") ? "/buyProject" : "/buyProject";
         setTimeout(() => {
-          navigate("/buyProject");
+          navigate(destination);
           setTimeout(() => {
             setSpeech({
               text: "Do you want me to read the titles for you?",
               language: "en-US",
-              voiceIndex: 5
+              voiceIndex: actor
             });
             setNewsPromptSpoken(true);
           }, 1000);
         }, 2500);
-      } else if (transcript === "GO TO CONSULTATION") {
-        setSpeech({
+      } else if (transcript.includes("GO TO CONSULTATION")) {
+        action = {
           text: "Yes, going right now.",
           language: "en-US",
-          voiceIndex: 5
-        });
+          voiceIndex: actor
+        };
         setTimeout(() => {
           navigate("/do-a-quick-consultation");
         }, 2500);
+      } else if (transcript.includes("LOG OUT") || transcript.includes("LOOK OUT.")) {
+        if (authData.user)
+        {
+          action = {
+            text: "Are you sure you want to log out.",
+            language: "en-US",
+            voiceIndex: actor
+          };
+          setLogoutSure(true)
+        }
+        else {
+          setSpeech({
+            text: "You are already disconnected",
+            language: "en-US",
+            voiceIndex: actor
+          });
+        }
+      }
+
+      if (action) {
+        setSpeech(action);
       }
     }
   }, [transcript, speech, navigate]);
-  
+
   useEffect(() => {
+    console.log(logoutSure)
     if (speech && transcript === "STOP") {
-      setSpeech(null); // Arrêter la lecture définitivement
-      setScroll(0); // Arrêter le défilement automatique
+      setSpeech(null);
+      setScroll(0);
     } else if (!speech) {
-      if (newsPromptSpoken && (transcript === "YES." || transcript === "YES")) {
+      if (newsPromptSpoken && (transcript === "YES." || transcript === "YES" || transcript === "YES, PLEASE.")) {
         setTitles(true);
         setNewsPromptSpoken(false);
         setScroll(300);
-      } else if (newsPromptSpoken && (transcript === "NO." || transcript === "NO")) {
+      } else if (newsPromptSpoken && (transcript === "NO." || transcript === "NO" ||  transcript === "NO, PLEASE.")) {
         setSpeech({
           text: "As you like, do you need something else?",
           language: "en-US",
-          voiceIndex: 5
+          voiceIndex: actor
         });
         setNewsPromptSpoken(false);
+      } else if (logoutSure && (transcript === "YES." || transcript === "YES" || transcript === "YES,PLEASE")) {
+   
+          setSpeech({
+            text: "You've been disconnected, see you later",
+            language: "en-US",
+            voiceIndex: actor
+          });
+          handleLogout();
+          setLogoutSure(false);
+        
+       
+
+      } else if (logoutSure && (transcript === "NO." || transcript === "NO" ||  transcript === "NO, PLEASE.")) {
+        setSpeech({
+          text: "Deconnexion failed",
+          language: "en-US",
+          voiceIndex: actor
+        });
+        setLogoutSure(false);
       }
     }
-  }, [transcript, newsPromptSpoken, speech]);
+  }, [transcript, newsPromptSpoken, speech, logoutSure]);
   
-  
-  
-
   useEffect(() => {
-      window.scrollTo({
-        top: scroll,
-        behavior: "smooth"
-      });
-    
-  }, [scroll]); 
+    window.scrollTo({
+      top: scroll,
+      behavior: "smooth"
+    });
+  }, [scroll]);
 
   useEffect(() => {
     if (titleService) {
       let delay = 0;
       services.forEach((service, index) => {
         setTimeout(() => {
-          if (index % 4 === 0 && index !== 0 ) 
-            setScroll((prevScroll) => prevScroll + 550); // Adjust the scroll position based on your needs
-            setSpeech({
-              text: service.title,
-              language: "en-US",
-              voiceIndex: 5,
-            });
+          if (index % 4 === 0 && index !== 0)
+            setScroll((prevScroll) => prevScroll + 550);
+          setSpeech({
+            text: service.title,
+            language: "en-US",
+            voiceIndex: actor,
+          });
         }, delay);
-  
-        delay += 3500;
-      });
-  
-      setTitles(false);
 
+        delay += 3800;
+      });
+
+      setTitles(false);
     }
   }, [titleService, services]);
+
   useEffect(() => {
     const normalizedTranscript = transcript.trim().toLowerCase();
     switch (normalizedTranscript) {
+      case "who are you?":
       case "who are you":
         setSpeech({
           text: "I am Alan, a computer program created by students. Devoid of personal experiences or identity, my purpose is to assist and provide information to the best of my abilities. How may I assist you today",
           language: "en-US",
-          voiceIndex: 5,
+          voiceIndex: actor,
         });
         break;
-        case "help":
+      case "hello.":
+      case "help.":
+      case "help":
         setSpeech({
-          text: "Hello, I'm Alan, a computer program. Here are some functionalities that I can provide, I can read text and navigate into pages for Exemple say go to Service.",
+          text: "Hello, I'm Alan, a computer program. Here are some functionalities that I can provide, I can read text and navigate into pages",
           language: "en-US",
-          voiceIndex: 5,
+          voiceIndex: actor,
         });
         break;
-        
-        case "thank you":
-          setSpeech({
-            text: "You are welcome",
-            language: "en-US",
-            voiceIndex: 5,
-          });
-          break;
-       
-      
+      case "thank you.":
+      case "thank you":
+        setSpeech({
+          text: "You are welcome",
+          language: "en-US",
+          voiceIndex: actor,
+        });
+        break;
+ 
     }
   }, [transcript]);
-  
+
   return (
     <section className="pt-24 w-full">
       <div>
