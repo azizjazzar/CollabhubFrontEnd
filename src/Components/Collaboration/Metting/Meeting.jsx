66e57af7a5b15dd4aca9a70792ee5526d541b2c7
ademnsir <nsir.adem@esprit.tn>
2024-03-02 15:39:59 +0100

    adem

--- src/Components/Collaboration/Metting/Meeting.jsx
+++ src/Components/Collaboration/Metting/Meeting.jsx
@@ -1,5 +1,9 @@
+// Meeting.jsx
+
 import React, { useRef, useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
+import { FaMicrophone, FaMicrophoneSlash, FaVideo, FaVideoSlash } from 'react-icons/fa';
+import '@/widgets/assets/meeting.css';
 
 function Meeting() {
   const webcamRef = useRef(null);
@@ -34,20 +38,33 @@ function Meeting() {
     };
   }, [isCameraOn, isMicroOn]);
 
+  // Mettre l'état initial à false pour cacher la webcam et désactiver le microphone
+  useEffect(() => {
+    setIsCameraOn(false);
+    setIsMicroOn(false);
+  }, []);
+
   return (
-    <div className="pt-24">
-      <div>
-        {isCameraOn ? (
-          <Webcam
-            audio={isMicroOn}
-            ref={webcamRef}
-            mirrored={true}
-          />
-        ) : (
-          <div>Webcam désactivée</div>
-        )}
-        <button onClick={toggleCamera}>{isCameraOn ? 'Désactiver la webcam' : 'Activer la webcam'}</button>
-        <button onClick={toggleMicro}>{isMicroOn ? 'Désactiver le micro' : 'Activer le micro'}</button>
+    <div className="meeting-container">
+      <div className="video-container">
+        <Webcam
+          audio={isMicroOn}
+          ref={webcamRef}
+          mirrored={true}
+          style={{ display: isCameraOn ? 'block' : 'none' }}
+        />
+        {!isCameraOn && <div className="webcam-off-message">Webcam désactivée</div>}
+      </div>
+      <div className="chat-sidebar">
+        {/* Contenu de la barre latérale de chat ici */}
+      </div>
+      <div className="bottom-navbar">
+        <button className="icon-button" onClick={toggleCamera}>
+          {isCameraOn ? <FaVideo className="icon" /> : <FaVideoSlash className="icon" />}
+        </button>
+        <button className="icon-button" onClick={toggleMicro}>
+          {isMicroOn ? <FaMicrophone className="icon" /> : <FaMicrophoneSlash className="icon" />}
+        </button>
       </div>
     </div>
   );
