dc3ca9245d5ff32974e3d63d53b28c1cd7d6bc70
rania hachem <rania.hachem@esprit.tn>
2024-05-03 10:46:18 +0100

    last version 3

--- src/Components/GServices/myRequests.jsx
+++ src/Components/GServices/myRequests.jsx
@@ -10,12 +10,11 @@ const MyRequests = () => {
 
   useEffect(() => {
     fetchRequests();
-  }, [serviceId]); // Re-fetch requests whenever serviceId changes
+  }, [serviceId]);
 
   const fetchRequests = async () => {
     try {
       const response = await axios.get(`https://colabhub.onrender.com/requests/${serviceId}`);
-   
       setRequests(response.data.clientRequests);
       setLoading(false);
     } catch (error) {
@@ -24,11 +23,10 @@ const MyRequests = () => {
       setLoading(false);
     }
   };
-  
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      <h1>My Requests</h1>
+    <main className="w-3/4 p-4 mx-auto pt-20 space-y-3"> {/* Ajout du padding en haut */}
+<h1 className="text-2xl pt-20 font-bold mb-4 text-orange-500">My Requests</h1>
       {loading ? (
         <p>Loading...</p>
       ) : error ? (
@@ -37,12 +35,19 @@ const MyRequests = () => {
         <div>
           {requests.length > 0 ? (
             requests.map((request) => (
-              <div key={request._id} className="request-card">
-                <p>Email: {request.email}</p>
-                <p>Instructions: {request.instructions}</p>
-                <Link to={`/postWork/${request.serviceId}`}>
-                  <button className="post-work-button">Post Your Work Now</button>
-                </Link>
+              <div key={request._id} className="bg-white rounded-lg shadow-md p-4 space-y-2 transition duration-300 ease-in-out hover:shadow-lg">
+                <div className="flex items-center">
+                  <div>
+                    {/* Here you can render the profile picture if available */}
+                  </div>
+                  <div className="flex-grow ml-2 pt-3">
+                    <h6 className="text-xl font-bold text-sm">Email: <span className="font-normal">{request.email}</span></h6>
+                    <p className="text-sm font-bold">Instructions: <span className="font-normal">{request.instructions}</span></p>
+                  </div>
+                  <Link to={`/postWork/${request.serviceId}`}>
+                    <button className="bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md focus:outline-none">Post Your Work Now</button>
+                  </Link>
+                </div>
               </div>
             ))
           ) : (
@@ -50,11 +55,8 @@ const MyRequests = () => {
           )}
         </div>
       )}
-    </div>
+    </main>
   );
-  
-  
-  
 };
 
 export default MyRequests;
